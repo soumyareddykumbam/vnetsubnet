6cd86e5dd16655dfe56ae01fccf5b3ec1e557e0f
soumya Donthi <soumyareddy.kumbam@gmail.com>
2023-07-09 14:27:04 +0100

    vnet and disk code

---
+++ main.tf
@@ -0,0 +1,27 @@
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = var.azurerm_virtual_network
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = var.address_space
+
+
+  subnet {
+    name           = var.subnet
+    address_prefix = var.address_prefix
+  }
+
+  
+}
+resource "azurerm_managed_disk" "example" {
+  name                 = var.azurerm_managed_disk
+  resource_group_name  = azurerm_resource_group.example.name
+  location             = azurerm_resource_group.example.location
+  storage_account_type = var.storage_account_type
+  disk_size_gb      = var.disk_size_gb
+
+}
---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "resource_group_name" {
+    type = string
+    description = "this is a resourcegroup name"
+  
+}
+variable "resource_group_location" {
+  
+}
+variable "azurerm_virtual_network" {
+  
+}
+variable "address_space" {
+  
+}
+variable "subnet" {
+  
+}
+variable "address_prefix" {
+  
+}
+variable "azurerm_managed_disk" {
+  
+}
+variable "storage_account_type" {
+  
+}
+variable "disk_size_gb" {
+  
+}
