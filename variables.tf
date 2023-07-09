bbe14e69ae1386372a85bc75d07ce142b94ba525
soumyareddykumbam <138958516+soumyareddykumbam@users.noreply.github.com>
2023-07-09 16:03:30 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,30 +1,30 @@
 variable "resource_group_name" {
-    type = "string"
+    type = string
     description = "this is a resourcegroup name"
   
 }
 variable "resource_group_location" {
-type = "string"
+type = string
   
 }
 variable "virtual_network_name" {
-type="string"
+type= string
   
 }
 variable "address_space" {
-type = "list"
+type = list
   
 }
 variable "subnet_name" {
-type = "string"
+type = string
   
 }
 variable "address_prefix" {
-type = "list"
+type = list
   
 }
 variable "managed_disk_name" {
-type = "string"
+type = string
   
 }
 variable "storage_account_type" {
@@ -32,7 +32,7 @@ type = string
   
 }
 variable "create_option" {
-type = "string"
+type = string
   
 }
 variable "disk_size_gb" {}
